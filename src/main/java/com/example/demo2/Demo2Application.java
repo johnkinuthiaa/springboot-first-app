2b09df4eb254f088dfe68f9f1d2416040de86876
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-09-14 23:11:18 +0300

    initial commit

---
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -0,0 +1,21 @@
+package com.example.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@RestController
+
+public class Demo2Application {
+    @GetMapping("/greetings")
+    public String greetings(@RequestParam(value = "name",defaultValue = "kingkade")String name){
+        return String.format("hello %s",name);
+    }
+    public static void main(String[] args){
+        SpringApplication.run(Demo2Application.class,args);
+    }
+}
+
